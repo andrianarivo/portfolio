becb3928017c6c82129ed286c49daf7f0883a2c6
Andrianarivo David <andrianarivo.david@gmail.com>
2023-04-10 22:07:04 +0300

    popup window desktop and mobile

--- script.js
+++ script.js
@@ -17,17 +17,35 @@ const showMenu = () => {
   }
 };
 
-window.addEventListener('resize', () => hideMenu());
+window.addEventListener('resize', hideMenu);
 
-openMenu.addEventListener('click', () => showMenu());
+openMenu.addEventListener('click', showMenu);
 
 const closeMenu = document.querySelector('#close-menu');
 
-closeMenu.addEventListener('click', () => hideMenu());
+closeMenu.addEventListener('click', hideMenu);
 
 const menuLinks = document.querySelectorAll('a.menu-link');
 for (let i = 0; i < menuLinks.length; i += 1) {
   menuLinks[i].addEventListener('click', () => {
     hideMenu();
   });
+}
+
+const closePopupIcon = document.querySelector('#close-popup-icon');
+const popupWindow = document.querySelector('#popup-window');
+const projectButtons = document.querySelectorAll('.projects .project-item .content .button-outlined');
+
+const openPopupWindow = () => {
+  popupWindow.style.visibility = 'visible';
+};
+
+const closePopupWindow = () => {
+  popupWindow.style.visibility = 'hidden';
+};
+
+closePopupIcon.addEventListener('click', closePopupWindow);
+
+for (let i = 0; i < projectButtons.length; i += 1) {
+  projectButtons[i].addEventListener('click', openPopupWindow);
 }
