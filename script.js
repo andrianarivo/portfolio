3bc36f33f144813950ab04bea262e91ebe7670c4
andrianarivo <andrianarivo.david@gmail.com>
2023-04-11 17:23:25 +0300

    changed projects to be dynamic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const projects = [
     name: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: 'images/project1_snapshot.png',
+    captions: ['CANOPY', 'Back End Dev', '2015'],
     technologies: ['HTML', 'CSS', 'Javascript'],
     linkToLive: '#',
     linkToSource: '#',
@@ -11,6 +12,7 @@ const projects = [
     name: 'Multi-Post Stories',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: 'images/project2_snapshot.png',
+    captions: ['CANOPY', 'Back End Dev', '2015'],
     technologies: ['HTML', 'CSS', 'JS'],
     linkToLive: '#',
     linkToSource: '#',
@@ -19,6 +21,7 @@ const projects = [
     name: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: 'images/project3_snapshot.png',
+    captions: ['CANOPY', 'Back End Dev', '2015'],
     technologies: ['HTML', 'CSS', 'JS'],
     linkToLive: '#',
     linkToSource: '#',
@@ -27,6 +30,7 @@ const projects = [
     name: 'Multi-Post Stories',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: 'images/project4_snapshot.png',
+    captions: ['CANOPY', 'Back End Dev', '2015'],
     technologies: ['HTML', 'CSS', 'JS'],
     linkToLive: '#',
     linkToSource: '#',
@@ -107,3 +111,82 @@ for (let i = 0; i < projectButtons.length; i += 1) {
     openPopupWindow(projects[i]);
   });
 }
+
+const loadProject = (projectItem) => {
+  const parent = document.createElement('div');
+  parent.className = 'project-item';
+
+  const img = document.createElement('img');
+  img.src = projectItem.featuredImage;
+  img.width = '295';
+  img.height = '220';
+  img.alt = `project ${projectItem.name}`;
+  parent.appendChild(img);
+
+  const content = document.createElement('div');
+  content.className = 'content';
+
+  const h3 = document.createElement('h3');
+  h3.innerHTML = projectItem.name;
+  h3.className = 'h1 text-bold';
+  content.appendChild(h3);
+
+  const captions = document.createElement('ul');
+  captions.className = 'captions';
+  for (let i = 0; i < projectItem.captions.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = projectItem.captions[i];
+    if (i === 0) {
+      li.className = 'text-bold';
+    } else {
+      li.className = 'text-medium text-light-grey';
+    }
+    captions.appendChild(li);
+    if (i !== projectItem.captions.length - 1) {
+      const counter = document.createElement('li');
+      const span = document.createElement('span');
+      span.className = 'counter';
+      counter.appendChild(span);
+      captions.appendChild(counter);
+    }
+  }
+  content.appendChild(captions);
+
+  const desc = document.createElement('p');
+  desc.innerHTML = projectItem.description;
+  content.appendChild(desc);
+
+  const projLang = document.createElement('ul');
+  projLang.className = 'project-language';
+  for (let i = 0; i < projectItem.technologies.length; i += 1) {
+    const li = document.createElement('li');
+    li.className = 'text-medium text-dark-purple';
+    li.innerHTML = projectItem.technologies[i];
+    projLang.appendChild(li);
+  }
+  content.appendChild(projLang);
+
+  const button = document.createElement('button');
+  button.innerHTML = 'See project';
+  button.className = 'button-outlined text-medium';
+  button.onclick = () => {
+    openPopupWindow(projectItem);
+  };
+  content.appendChild(button);
+
+  parent.appendChild(content);
+
+  return parent;
+};
+
+const loadProjects = () => {
+  const projectsElement = document.querySelector('.projects');
+  for (let i = 0; i < projects.length; i += 1) {
+    const projectItemElement = loadProject(projects[i]);
+    projectsElement.appendChild(projectItemElement);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadProjects();
+});
