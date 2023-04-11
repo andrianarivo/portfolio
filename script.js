1e167de670d012d75f89255ff39471ba92243d8f
andrianarivo <andrianarivo.david@gmail.com>
2023-04-11 15:56:03 +0300

    using js to store data and display them

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const projects = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'images/project1_snapshot.png',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    linkToLive: '#',
+    linkToSource: '#',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'images/project2_snapshot.png',
+    technologies: ['HTML', 'CSS', 'JS'],
+    linkToLive: '#',
+    linkToSource: '#',
+  },
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'images/project3_snapshot.png',
+    technologies: ['HTML', 'CSS', 'JS'],
+    linkToLive: '#',
+    linkToSource: '#',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'images/project4_snapshot.png',
+    technologies: ['HTML', 'CSS', 'JS'],
+    linkToLive: '#',
+    linkToSource: '#',
+  },
+];
+
 const openMenu = document.querySelector('div#open-menu');
 const mobileMenu = document.querySelector('div#mobile-menu');
 
@@ -26,6 +61,7 @@ const closeMenu = document.querySelector('#close-menu');
 closeMenu.addEventListener('click', hideMenu);
 
 const menuLinks = document.querySelectorAll('a.menu-link');
+
 for (let i = 0; i < menuLinks.length; i += 1) {
   menuLinks[i].addEventListener('click', () => {
     hideMenu();
@@ -33,10 +69,30 @@ for (let i = 0; i < menuLinks.length; i += 1) {
 }
 
 const closePopupIcon = document.querySelector('#close-popup-icon');
+
 const popupWindow = document.querySelector('#popup-window');
+
 const projectButtons = document.querySelectorAll('.projects .project-item .content .button-outlined');
 
-const openPopupWindow = () => {
+const openPopupWindow = (projectItem) => {
+  const projectName = document.querySelector('#project-name');
+  const projectDesc = document.querySelector('#project-description');
+  const projectImage = document.querySelector('#project-image');
+  const projectTechs = document.querySelectorAll('#project-technologies li');
+  const projectLinkToLive = document.querySelector('#link-to-live');
+  const projectLinkToSource = document.querySelector('#link-to-source');
+
+  projectName.innerHTML = projectItem.name;
+  projectDesc.innerHTML = projectItem.description;
+  projectImage.src = projectItem.featuredImage;
+
+  for (let i = 0; i < projectTechs.length; i += 1) {
+    projectTechs[i].innerHTML = projectItem.technologies[i];
+  }
+
+  projectLinkToLive.href = projectItem.linkToLive;
+  projectLinkToSource.href = projectItem.linkToSource;
+
   popupWindow.style.visibility = 'visible';
 };
 
@@ -47,5 +103,7 @@ const closePopupWindow = () => {
 closePopupIcon.addEventListener('click', closePopupWindow);
 
 for (let i = 0; i < projectButtons.length; i += 1) {
-  projectButtons[i].addEventListener('click', openPopupWindow);
+  projectButtons[i].addEventListener('click', () => {
+    openPopupWindow(projects[i]);
+  });
 }
